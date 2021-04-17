5adb4083d77abb6ef6e150dbd280ee8a85dfb49d
giangtran2k <tranquocgjang@gmail.com>
2021-04-17 16:13:10 +0700

    second commit

--- xlm8.js
+++ xlm8.js
@@ -1,12 +1,22 @@
 const arr = [1, 2, 3, 4,6,3,4,5,3,4,2];
+
 console.log(arr);
-function chenPhanTu(x,k){
-    if(k>arr.length||k<0){
-        console.log("gia tri k khong hop le");
-    }
-    else{
-        
-        
-    }
-    
+function chenPhanTu(k,x){
+
+	
+
+
+	Array.prototype.insert = function ( index, item ) {
+    this.splice( index, 0, item );
+	};
+	// var arr = [1, 2, 3, 4,6,3,4,5,3,4,2];
+	arr.insert(k, x);
+	console.log(arr);
+   
+
 }
+chenPhanTu(5,100);
+
+
+
+
