f31793fb45911ada5ccb87fe595fbadc2c69ad67
giangtran2k <tranquocgjang@gmail.com>
2021-04-19 09:54:32 +0700

    tring

---
+++ string1.js
@@ -0,0 +1,7 @@
+var txt = "Tran Quoc giang";   // String
+
+for (let index = txt.length; index >=0; index--) {
+
+  console.log(txt[index]);
+  
+}
---
+++ string2.js
@@ -0,0 +1,9 @@
+var txt = "Tran Quoc giang";   // String
+var a = txt.split(" ");   // Split on commas
+
+for (let index = 0; index < a.length; index++) {
+
+  console.log(a[index])
+  
+}
+console.log(a)
